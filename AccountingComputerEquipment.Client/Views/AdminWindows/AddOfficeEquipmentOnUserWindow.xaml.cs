184c4dba2792a94e1dddf4e8d659b92b6e14776a
aniqoff-DEV <andrey.emailforwork.dev@gmail.com>
2024-03-25 20:09:27 +0300

    transfer information from a comboBox to a textBox

--- AccountingComputerEquipment.Client/Views/AdminWindows/AddOfficeEquipmentOnUserWindow.xaml.cs
+++ AccountingComputerEquipment.Client/Views/AdminWindows/AddOfficeEquipmentOnUserWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using AccountingComputerEquipment.Client.Models;
 using AccountingComputerEquipment.Client.ViewModels.AdminViewModels;
 using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace AccountingComputerEquipment.Client.Views.AdminWindows
 {
@@ -23,6 +24,15 @@ namespace AccountingComputerEquipment.Client.Views.AdminWindows
             officeEquipmentsBox.Visibility = Visibility.Hidden;
             createButton.IsEnabled = false;
             createButton.Opacity = 0.5;
+        }
+
+        private void officeEquipmentsBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            var officeEquipment = officeEquipmentsBox.SelectedItem as OfficeEquipment;
+
+            photo.Source = new BitmapImage(new Uri(officeEquipment.Photo));
+            textBoxOE.Text = $"{officeEquipment.Name}" +
+                $"\n{officeEquipment.Cost}\n{officeEquipment.Description}";
         }
     }
 }
